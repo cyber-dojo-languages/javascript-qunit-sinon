084dbfeda3c620ba3b0816afe10f9b20565a8c33
Jon Jagger <jon@jaggersoft.com>
2020-06-22 18:07:27 +0100

    [ci skip] Delete start_point/ dir

--- start_point/hiker-test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const hiker = require('./hiker.js');
-
-QUnit.test('answer', function(assert) {
-    const info = 'to life the universe and everything';
-    assert.equal(hiker.answer(), 42, info);
-});
